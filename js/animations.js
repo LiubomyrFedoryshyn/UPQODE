2d4afceb01b42ec92931e656a0b8d58aff2c8424
robber <robber995@gmail.com>
2018-12-16 15:42:07 +0200

    added the animations and fixed scill scalle bug

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     
+
+// carousel interval;
     $('.carousel').carousel({
         interval: 7000
       })
@@ -11,6 +13,8 @@ $(document).ready(function(){
         document.getElementById('animate4').classList.add('filed4');        
       });
 
+
+//form filling;
       $('.myForm').submit(function (eventObject){
           if($('.email').val() == "" ) {
               eventObject.preventDefault();
@@ -24,9 +28,45 @@ $(document).ready(function(){
             eventObject.preventDefault();
             alert("Please, put a trigger on a box");
         } 
-    
-          
       });
-});
+
+//animations;
+      window.sr = ScrollReveal({ reset: true });
+
+      sr.reveal('.foo-1', {
+          origin: 'bottom',
+          mobile: true, 
+          duration: 2000
+      })
+
+      sr.reveal('.foo-2', { 
+        origin: 'right',
+        mobile: true, 
+        duration: 2000 
+      });
+
+      sr.reveal('.foo-3', { 
+        rotate: { x: 100, y: 100, z: 0 },
+        mobile: true, 
+        duration: 1000
+      });
+
+    sr.reveal('.foo-4', {
+        duration: 2000,
+        mobile: true, 
+        origin: 'bottom'
+    });
+
+    sr.reveal('.foo-5', {
+        duration: 2000,
+        origin: 'left',
+        distance: '300px',
+        mobile: true, 
+        viewFactor: 0.2
+    });
+    
+
+    });//end of ready;
 
 
+//smooth scrolling;
